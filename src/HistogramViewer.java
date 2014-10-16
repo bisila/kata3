8f0133a5a0b1647612ddfae655d9d1261ef99c97
usuario <usuario@10.22.143.28>
2014-10-16 12:10:33 +0100

    First version of kata 3 (J.16.10.14)

---
+++ src/HistogramViewer.java
@@ -0,0 +1,7 @@
+public class HistogramViewer <Type>{
+    public void print (Histogram<Type> histogram){
+        for (Object key : histogram.keySet()) {
+            System.out.println(key + "-->" + histogram.get(key));
+        }
+    }
+}
